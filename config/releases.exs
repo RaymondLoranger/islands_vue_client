4df99a40262aecbe56c246c3f136b39a32d81402
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-15 11:51:05 -0500

    setup for gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -1,9 +1,13 @@
 import Config
 
-if config_env() == :prod do
-  config :islands_vue_client, Islands.Vue.ClientWeb.Endpoint,
-    server: true,
-    # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-    http: [port: {:system, "PORT"}],
-    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+case config_env() do
+  :prod ->
+    config :islands_vue_client, Islands.Vue.ClientWeb.Endpoint,
+      server: true,
+      # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+      http: [port: {:system, "PORT"}],
+      url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+
+  _else ->
+    config :islands_vue_client, Islands.Vue.ClientWeb.Endpoint, server: true
 end
